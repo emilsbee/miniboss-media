6f4cc6a6dd4ffad746e80d9a99f25694af9ffc30
Emils Bernhards <bernhards@emils.xyz>
2022-10-23 18:45:05 +0200

    Fix styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,28 +35,30 @@ export default function Home() {
   };
   
   return (
-    <div className="w-full h-full flex flex-col justify-start items-center p-10">
-      <div className="max-w-7xl	w-full h-full flex flex-col justify-between items-start">
-        <Head>
-          <title>Miniboss</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        <div className="max-w-7xl flex justify-center items-end flex-wrap gap-x-10 gap-y-6 mb-6">
-          <Image priority src="/images/miniboss.png" width={75} height={150} alt="Miniboss" />
-          <div className="absolute top-0 right-[40px]">
-            <Image priority src="/images/minions-hanging.png" width={55} height={140} alt="Miniboss" />
+    <>
+      <div className="w-full flex flex-col justify-start items-center p-10 pb-[90px]">
+        <div className="max-w-7xl	w-full flex flex-col justify-between items-start">
+          <Head>
+            <title>Miniboss</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
+          <div className="max-w-7xl flex justify-center items-end flex-wrap gap-x-10 gap-y-6 mb-6">
+            <Image priority src="/images/miniboss.png" width={75} height={150} alt="Miniboss" />
+            <div className="absolute top-0 right-[40px]">
+              <Image priority src="/images/minions-hanging.png" width={55} height={140} alt="Miniboss" />
+            </div>
+            <h1 className="font-bold text-2xl text-slate-300">
+              Minibosa filmas un seriali
+            </h1>
           </div>
-          <h1 className="font-bold text-2xl text-slate-300">
-            Minibosa filmas un seriali
-          </h1>
+          <div className="w-full flex flex-col gap-y-3">
+            {media && media.sort((a, b) => a.title.localeCompare(b.title)).map((media) => (
+              <MediaItem key={media.id} media={media} />
+            ))}
+          </div>
         </div>
-        <div className="w-full h-full flex flex-col gap-y-3">
-          {media && media.sort((a, b) => a.title.localeCompare(b.title)).map((media) => (
-            <MediaItem key={media.id} media={media} />
-          ))}
-        </div>
+        <AddMediaForm onAddMedia={handleAddMedia} />
       </div>
-      <AddMediaForm onAddMedia={handleAddMedia} />
-    </div>
+    </>
   )
 }
